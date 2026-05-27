3e16e4721e55cd12cf9b306da197ee9993904d6f
Jan Majewski <j.m.majewski@student.tue.nl>
2026-05-27 12:41:47 +0200

    add_path.py is done; 8.1 tests passed

--- src/add_pose.py
+++ src/add_pose.py
@@ -9,8 +9,21 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # type: ignore # (bearing, range)
 
 def add_pose(graph, initial_estimate):
-    # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
+    # TODO: Add the odometry factor between X(3) and X(4) to the graph (BetweenFactorPose2)
+    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(2**0.5, 2**0.5, math.pi/2), ODOMETRY_NOISE)) # type: ignore
 
     # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
+
+    # initial_estimate.insert(X(1), gtsam.Pose2(-0.25, 0.20, 0.15))
+    # initial_estimate.insert(X(2), gtsam.Pose2(2.30, 0.10, -0.20))
+    # initial_estimate.insert(X(3), gtsam.Pose2(4.10, 0.10, 0.10))
+    prevPos = np.array([4, 0, 0])
+    movement = np.array([2 * math.cos(math.pi/4), 2 * math.sin(math.pi/4), math.pi/2])
+    nextPos = prevPos + movement
+    # print(prevPos)
+    # print(movement)
+    # print(nextPos)
+
+    initial_estimate.insert(X(4), gtsam.Pose2(nextPos[0], nextPos[1], nextPos[2])) # type: ignore
     
     return graph, initial_estimate
